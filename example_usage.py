eb28206d552ff2f755a0c9063aa2a0b3f14cf43c
Kyle Conroy <kyleconroy@gmail.com>
2016-10-18 21:53:00 -0400

    updated example for clarity

--- example_usage.py
+++ example_usage.py
@@ -5,7 +5,9 @@ logger = phoebe.logger(clevel='INFO')
 
 b = phoebe.default_binary()
 
-b.add_compute('customcompute')
+b.add_compute(pluginexample.compute.customcompute)
+# or could have even just done the following:
+#b.add_compute('customcompute')
 b.add_dataset('lc', times=[0,1,2,3])
 b.add_dataset('rv', times=[0,1,2,3])
 b.run_compute()
